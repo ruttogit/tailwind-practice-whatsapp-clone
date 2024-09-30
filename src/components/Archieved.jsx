c0198d40a2a1b6e1a4adfe540aabb7a5ea8a26da
Dennis Kipkoech <kipkoechd09@gmail.com>
2024-09-30 09:26:06 +0300

    made changes in archieved component

--- src/components/Archieved.jsx
+++ src/components/Archieved.jsx
@@ -3,10 +3,16 @@ import archieve from ".././images/archive.svg"
 
 function Archieved() {
   return (
-    <div className='py-4 px-8 flex items-center gap-4'>
-        <img src={archieve} alt="" className='w-5'/>
-        <h3 className='font-semibold'>Archived</h3>
-    </div>
+    <> 
+      <div className='my-0 h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10'>
+
+      </div>
+      <div className='py-4 px-8 flex items-center gap-4' >
+          <img src={archieve} alt="" className='w-5'/>
+          <h3 className='font-semibold'>Archived</h3>
+      </div>
+    </>
+
   )
 }
 
